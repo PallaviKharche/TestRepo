97c62bc8121fc714dddc96efac534bc06b9ccc18
pallavi.deshmukh9663 <pallavi.deshmukh9663@gmail.com>
2019-11-21 15:30:36 +0530

    Discharge Summary

---
+++ dischargeSummuryContoller.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('bahmni.clinical')
+    .controller('dischargeSummuryContoller', ['$scope', '$rootScope',
+        function ($scope, $rootScope) {
+            $scope.isTarvReport = $rootScope.isTarvReport;
+            $scope.data = $rootScope.dischargeSummuryReportData;
+        }]);
+
+        
